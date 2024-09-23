d78db1d958981f9056c3e33666f9b6d3d9d5cc81
Abu Kalam <akwebdev69@gmail.com>
2024-09-23 23:19:03 +0600

    some issues solved

--- js/script.js
+++ js/script.js
@@ -27,12 +27,11 @@ function box1() {
         const modal = document.getElementById('modal');
         modalCloseBtn.addEventListener('click', function(){
             modal.classList.remove("click-modal");
-        })
+        }) 
       
 
         modal.classList.add("click-modal");
         
-
 
         
       }
@@ -45,6 +44,35 @@ function box1() {
   }
   
   box1();
+
+
 //   
 // 
+function donationHistory (){
+const donatiionBnt = document.getElementById('donation-btn');
+const historyBtn = document.getElementById('history-btn');
+const donateSection = document.getElementById('donate-section');
+const historySection = document.getElementById('history-section')
+
+donatiionBnt.addEventListener('click', function(){
+historySection.classList.add("hidden")
+donateSection.classList.remove('hidden')
+historyBtn.style.backgroundColor = '#fff'
+donatiionBnt.style.backgroundColor = '#b4f461'
+donatiionBnt.style.border = 'none'
+
+
+
+
+})
+historyBtn.addEventListener('click', function(){
+    historySection.classList.remove("hidden");
+    donateSection.classList.add('hidden');
+    historyBtn.style.backgroundColor = '#B4F461';
+    donatiionBnt.style.backgroundColor = '#fff'
+    donatiionBnt.style.border = '1px solid rgba(17, 17, 17, 0.3)'
+    
+    })
+}
+donationHistory()
 // 
